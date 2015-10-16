2b988a811ab479c632a87a7a11a3392e461602e9
caixuehao <duole@duoledeMac-mini.local>
2015-10-16 18:51:10 +0800

    1.0.0.3

--- DMlist/DMlist/界面/分组显示界面/FZlistVC.m
+++ DMlist/DMlist/界面/分组显示界面/FZlistVC.m
@@ -8,53 +8,116 @@
 
 #import "FZlistVC.h"
 #import "DMlist.h"
+#import "LoginVC.h"
+#import "UserInfo.h"
+#import "FenZuCell.h"
+#import "DMTableVC.h"
+
 @interface FZlistVC ()
 
 @end
 
 @implementation FZlistVC
+//显示视图时调用刷新
+- (void)viewWillAppear:(BOOL)animated{
+    [_MainCollectionView reloadData];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//====数据===
+    //初始化
+    _FZARR = [[NSMutableArray alloc] init];
+    
+    
+    
+//====UI====
+    //导航栏
+    CGSize titleSize = self.navigationController.navigationBar.bounds.size;  //获取Navigation Bar的位置和大小
+    UIImageView* barbg = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, titleSize.width, titleSize.height)];
+    barbg.image = [UIImage imageNamed:@"acfun.jpg"];
+    [self.navigationController.navigationBar addSubview:barbg];
+    //集合视图
+    _w = ([[UIScreen mainScreen] bounds].size.width-30)/2;
+    [_MainCollectionView registerClass:[FenZuCell class] forCellWithReuseIdentifier:@"FenZuCell"];//注册Cell
+    
+    
+    
+    
+//====逻辑====
+    //先判断是否自动登录
+    BOOL bl = [UserInfo autologin];
+    NSMutableArray* userArr = [UserInfo Users];
+    if (bl) {
+        //验证第一个账号的密码账号
+        NSMutableDictionary* dic = userArr[0];
+        [UserInfo LoginUsername:[dic valueForKey:@"用户名"] Password:[dic valueForKey:@"密码"] Success:^{
+            
+            NSLog(@"登录成功");
+            [DMlist GetFZList:^(NSMutableArray *arr) {
+                _FZARR = arr;
+                 [_MainCollectionView reloadData];
+            }];
+            
+        } Fail:^(int i) {
+            NSLog(@"登录失败");//0 == 没有这个账号  1 == 密码错误
+            [self.navigationController pushViewController:[[LoginVC alloc] init] animated:YES];
+        }];
+    }else{
+        [self.navigationController pushViewController:[[LoginVC alloc] init] animated:YES];
+    }
+}
 
-    // Do any additional setup after loading the view from its nib.
-    [DMlist GetFZList:^(NSMutableArray *arr) {
-//        NSLog(@"%@",arr);
-        for (NSMutableDictionary *dic in [arr[0] valueForKey:@"元素数组"]) {
-            NSLog(@"%@",[dic valueForKey:@"名字"]);
-        }
-    }];
-//    [DMlist addF_name:@"一周的朋友" remark:@"测试" type:4 titleImageUrl:@"" FZ:0 Success:nil Fail:nil];
-}
-- (IBAction)btn1:(id)sender {
-//    [DMlist addF_name:_tf1.text remark:@"测试" type:4 titleImageUrl:@"" FZ:0 Success:^{
-//        [DMlist GetFZList:^(NSMutableArray *arr) {
-//            NSLog(@"%@",arr);
-//        }];
-//    } Fail:nil];
-//    [DMlist removeF:@"一周的朋友2" Success:nil Fail:nil];
-    [DMlist GetFZList:^(NSMutableArray *arr) {
-        //        NSLog(@"%@",arr);
-        for (NSMutableDictionary *dic in [arr[0] valueForKey:@"元素数组"]) {
-            NSLog(@"%@",[dic valueForKey:@"名字"]);
-        }
-    }];
-    [DMlist setF_oldname:@"08f4fca3e9" Name:_tf1.text remark:nil type:4 titleImageUrl:nil Success:nil Fail:nil];
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - CollectionView
+//定义展示的UICollectionViewCell的个数
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return _FZARR.count;
 }
-*/
+//定义展示的Section的个数
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+//定义每个UICollectionView 的大小
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    return CGSizeMake(_w, _w);
+}
+
+
+//每个UICollectionView展示的内容
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    //正常的单元格
+    FenZuCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"FenZuCell" forIndexPath:indexPath];
+    [cell setData:_FZARR[indexPath.row]];
+    return cell;
+}
+
+//定义每个UICollectionView 的 margin
+-(UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+    return UIEdgeInsetsMake(10, 10, 10, 10);
+}
+
+//UICollectionView被选中时调用的方法
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    DMTableVC* dmvc = [[DMTableVC alloc] initDIC:_FZARR[indexPath.row]];
+    [self.navigationController pushViewController:dmvc animated:YES];
+}
+
 
 @end
